817a53d1e80f4ccecd49893ba70d54af0b4e829a
eray-onur <eray.onur.developer@gmail.com>
2025-01-12 01:40:26 +0300

    Delete contact info service and test cases are implemented.

--- Phonebook.Directory/Application/Models/ErrorMessages.cs
+++ Phonebook.Directory/Application/Models/ErrorMessages.cs
@@ -3,6 +3,7 @@
     public class ErrorMessages
     {
         public const string FailedToFindPerson = "Failed to find a person with the given id.";
+        public const string FailedToFindContactInfo = "Failed to find the contact info with the given id.";
         public const string InvalidPhoneNumber = "Invalid phone number.";
     }
 }
