cdd52b0a3e1432322ce207452e6632e406c002cf
eray-onur <eray.onur.developer@gmail.com>
2025-01-11 19:36:38 +0300

    Person and Contact Info added.
    Added "Person" and "ContactInfo" domains.
    
    Added db context references and a db migration file for the previously mentioned entities.

---
+++ Phonebook.Directory/Domain/ContactInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Phonebook.Directory.Domain
+{
+    public class ContactInfo
+    {
+        public Guid Id { get; set; }
+        public Guid PersonId { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Email { get; set; }
+        public required string Location { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Phonebook.Directory/Persistence/Migrations/20250111163400_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Phonebook.Directory.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contact_infos",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    person_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_contact_infos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "people",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    company_name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_people", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contact_infos");
+
+            migrationBuilder.DropTable(
+                name: "people");
+        }
+    }
+}
