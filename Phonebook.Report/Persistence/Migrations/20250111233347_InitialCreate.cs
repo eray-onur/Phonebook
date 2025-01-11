b2154afb2c76a80c48eb3a3dd484a0e6100a2775
eray-onur <eray.onur.developer@gmail.com>
2025-01-12 02:34:55 +0300

    Added migration files and temporarily changing connection string to work in localhost.

---
+++ Phonebook.Report/Persistence/Migrations/20250111233347_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Phonebook.Report.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "reports",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    request_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    report_status = table.Column<int>(type: "integer", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    person_count = table.Column<long>(type: "bigint", nullable: true),
+                    phone_count = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_reports", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reports");
+        }
+    }
+}
