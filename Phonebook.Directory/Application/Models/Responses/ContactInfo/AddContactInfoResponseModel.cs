1a8e0544dfd1ff29bb7cd55b6c61152190de8600
eray-onur <eray.onur.developer@gmail.com>
2025-01-12 00:54:47 +0300

    Add contact info is service and its tests are developed.

---
+++ Phonebook.Directory/Application/Models/Responses/ContactInfo/AddContactInfoResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Phonebook.Directory.Application.Models.Responses.ContactInfo
+{
+    public class AddContactInfoResponseModel
+    {
+        public Guid AddedContactInfoId { get; set; }
+    }
+}
---
+++ Phonebook.Directory/Controllers/ContactInfoController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Phonebook.Directory.Application.Commands.ContactInfo;
+using Phonebook.Directory.Application.Models.Requests;
+using Phonebook.Directory.Application.Models.Requests.ContactInfo;
+using Phonebook.Directory.Persistence;
+
+namespace Phonebook.Directory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactInfoController : ControllerBase
+    {
+
+        private readonly IMediator mediator;
+
+        public ContactInfoController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create([FromBody] AddContactInfoRequestModel request)
+        {
+            var result = await mediator.Send(new AddContactInfoCommand(request));
+            return Ok(result);
+        }
+    }
+}
